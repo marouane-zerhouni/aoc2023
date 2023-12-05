89ee841fd0f96d536f7c0a8ec15c1ad756047241
marouane-zerhouni <marouanezer@gmail.com>
2023-12-06 00:00:27 +0100

    first star

--- day2.py
+++ day2.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import re
 
-=======
->>>>>>> 68a6e5183d43f6120b4ae9400f87e371cdf6308b
 def day2data(filename: str) -> list:
     with open(filename) as text_input:
         input_list = text_input.read().strip().split("\n")
@@ -11,7 +8,6 @@ def day2data(filename: str) -> list:
 
 day2puzzle = day2data("day2input")
 
-<<<<<<< HEAD
 def firststar(puzzle_input: list) -> int:
 
     class impossible_line(Exception): pass
@@ -37,16 +33,9 @@ def firststar(puzzle_input: list) -> int:
                         colour_value = colour_result[:colour_result.find(' ')]
                         if int(colour_value) > j:
                             max_score -= int(game_id)
-                            print('Max score is ' + str(max_score))
-                            print('Game id is ' + game_id)
-                            print('Colour value is ' + colour_value)
-                            print('Colour max is ' + str(j))
                             raise impossible_line
         except impossible_line:
             pass                  
     return max_score
 
 firststar(day2puzzle)
-=======
-print(day2puzzle)
->>>>>>> 68a6e5183d43f6120b4ae9400f87e371cdf6308b
